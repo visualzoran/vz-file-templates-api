8bdd072cf034d5b4110a8ccae8366f0c0080ee41
visualzoran <39418724+visualzoran@users.noreply.github.com>
2018-08-26 13:44:31 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# vz-file-templates-api
+# VZ File Templates Api
+
+NPM package containing public interfaces exposed by VZ File Templates extension for Visual Studio Code
+
---
+++ lib/vzFileTemplatesInterfaces.d.ts
@@ -0,0 +1,21 @@
+declare namespace vzFileTemplates {
+    interface IProjectItemTemplateRunSettings {
+        destPath: string;
+        getTextReplacement(key: string): string;
+        setTextReplacement(key: string, value: string): void;
+        getInputNameVariable(): string;
+        getFileInputNameVariable(): string;
+        applyReplacements(value: string): string;
+    }
+    interface IProjectItemTemplate {
+        run(settings: IProjectItemTemplateRunSettings): boolean;
+    }
+    interface IProjectItemWizard {
+        getName(): string;
+        run(template: IProjectItemTemplate, settings: IProjectItemTemplateRunSettings): void;
+    }
+    interface IVZFileTemplatesApi {
+        registerTemplatesFolder(folderPath: string): void;
+        registerWizard(wizard: IProjectItemWizard): void;
+    }
+}
